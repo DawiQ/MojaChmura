05b9d00d0f79ca0a940680d576b1809477fbae8c
Kamil Żarski <209526@edu.p.lodz.pl>
2019-06-10 10:53:56 +0200

    .

--- index.php
+++ index.php
@@ -130,11 +130,12 @@ if( isset( $_GET['logout'] ) ){
 				
 				<nav aria-label="Page navigation example">
 					<ul class="pagination">
-						<li class="page-item"><a class="page-link" href="#">Previous</a></li>
+						<li class="page-item"><a class="page-link" href="#"><</a></li>
 						<li class="page-item"><a class="page-link" href="#">1</a></li>
 						<li class="page-item"><a class="page-link" href="#">2</a></li>
 						<li class="page-item"><a class="page-link" href="#">3</a></li>
-						<li class="page-item"><a class="page-link" href="#">Next</a></li>
+						<li class="page-item"><a class="page-link" href="#">4</a></li>
+						<li class="page-item"><a class="page-link" href="#">></a></li>
 					</ul>
 				</nav>
 			</div>
